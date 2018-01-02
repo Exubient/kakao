a6d3ec422a682d5831a32339446e6c58599c5a9c
lcgkiller <lcgkiller@gmail.com>
2018-01-02 23:58:09 +0900

    error correction

--- alpaca/views.py
+++ alpaca/views.py
@@ -13,17 +13,19 @@ def keyboard(request):
 def answer(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
-    market = received_json_data['content']
-    
-    return JsonResponse({
-            'message': {
-                'text': 'test worked'
-            },
-            'keyboard': {
-                'type': 'buttons',
-                'buttons': ['B1', 'B2', 'B3']
-            }
+    return_str = received_json_data['content']
 
-        })
+    requestMode = return_str.encode('utf-8')  # utf-8형식으로 인코딩하여 한글을 인식
+
+    if requestMode == "B1":
+        return JsonResponse({
+                'message': {
+                    'text': 'test worked'
+                },
+                'keyboard': {
+                    'type': 'buttons',
+                }
+
+            })
 
 
