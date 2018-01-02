1fd14da832fe48051e40524c243160a9a4e23bbb
Ubuntu <ubuntu@ip-172-31-4-138.ap-northeast-2.compute.internal>
2018-01-02 03:57:52 +0000

    first commit

---
+++ alpaca/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def keyboard(request):
+
+    return JsonResponse({
+        'type' : 'buttons',
+        'buttons' : ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']
+    })
+
+
+
