162b4e4a6407a1af7c822deeb41f0a1c71effeeb
EC2 Default User <ec2-user@ip-172-31-22-128.ap-southeast-1.compute.internal>
2018-01-03 18:53:42 +0000

    change in views file

--- alpaca/views.py
+++ alpaca/views.py
@@ -1,34 +1,45 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json, datetime
+import json, datetime, csv
 
 def keyboard(request):
 	return JsonResponse({
 		'type' : 'buttons',
 		'buttons' : ['Coinone', 'Bithumb', 'Bitfinex']
 		})
-
 @csrf_exempt
 def answer(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
-    market = received_json_data['content']
-    
+    returnButton = received_json_data['content']
     with open(r'coin.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            print ', '.join(row)
+            info = row[0].split("-")
+            ret = info[0]
+            if info[0] == returnButton:
+                return JsonResponse({
+                    'message': {
+                        'text': "you have selected " + returnButton
+                     },
+                    'keyboard': {
+                        'type': 'buttons',
+                        'buttons': ['BTC', 'ETH', 'XRP']
+                    }
 
-    return JsonResponse({
-            'message': {
-                'text': market
-            },
-            'keyboard': {
-                'type': 'buttons',
-                'buttons': ['Coinone', 'Bithumb', 'Bitfinex']
-            }
+                })
+            if info[1] == returnButton and ret == info[0]: 
+                ret = ""
+                return JsonResponse({
+                    'message': {
+                        'text': row[1]
+                     },
+                    'keyboard':{
+                        'type': 'buttons',
+                        'buttons' : ['Coinone', 'Bithumb', 'Bitfinex']
 
-        })
+                        }
+                })
 
 
