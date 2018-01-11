002e28fbb7bd7c2c67ef08f66a1e8b0b8eaf8fb7
Hyun Joong Kim <henrynicholas09@gmail.com>
2018-01-11 11:48:34 +0900

    Update views.py
    주석

--- alpaca/views.py
+++ alpaca/views.py
@@ -7,7 +7,7 @@ ret ={}
 def keyboard(request):
     return JsonResponse({
         'type' : 'buttons',
-        'buttons' : ['Coinone', 'Bithumb', 'Bitfinex']
+        'buttons' : ['Coinone', 'Bithumb', 'Bitfinex'] #buttons shown at first, default API test
         })
 
 @csrf_exempt
@@ -19,17 +19,17 @@ def answer(request):
     with open(r'coin.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            info = row[0].split("-")
-            if(returnButton == info[0]):
-                global ret
-                ret[_dict["user_key"]] = info[0]
+            info = row[0].split("-") #info[0] 시장, info[1]가상화폐 종류, info[2] 단위
+            if(returnButton == info[0]):  # 첫번째로 보일 키보드, 시장을 선택 했을때 True
+                global ret #Database에 저장해야 되지만 세미나 특성상 state를 이렇게 처리합니다
+                ret[_dict["user_key"]] = info[0] #방금 유저가 어떤 시장을 눌렀을지 저장
                 return JsonResponse({
                     'message': {
-                        'text': "you have selected " + returnButton
+                        'text': "you have selected " + returnButton #first response from button
                      },
                     'keyboard': {
                         'type': 'buttons',
-                        'buttons': ['BTC', 'ETH', 'XRP']
+                        'buttons': ['BTC', 'ETH', 'XRP'] #buttons shown after first response
                     }
 
                 })
@@ -37,11 +37,11 @@ def answer(request):
             if(returnButton == info[1] and ret[_dict["user_key"]] == info[0]):
                 return JsonResponse({
                     'message': {
-                        'text': row[1] + info[2]
+                        'text': row[1] + info[2] #second response from button
                      },
                     'keyboard':{
                         'type': 'buttons',
-                        'buttons' : ['Coinone', 'Bithumb', 'Bitfinex']
+                        'buttons' : ['Coinone', 'Bithumb', 'Bitfinex'] #set of buttons shown after second response
 
                         }
                 })
